61ccb20353815b7e8c493bfa6a94aaf1070a879c
Chris Brown <dcbrown06@gmail.com>
2022-03-16 17:58:00 -0400

    Adding test file

---
+++ test/search.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const { expect }  = require('chai');
+
+describe('Duck Duck Go search using basic Puppeteer', function () {
+
+    let browser;
+    let page;
+
+    this.timeout(5000000);
+
+    beforeEach(async () => {
+        browser = await puppeteer.launch({headless:true});
+        page = await browser.newPage();
+
+        await page.goto('https://duckduckgo.com', {waitUntil: 'networkidle0'});
+    });
+
+    afterEach(async () => {
+        await browser.close();
+    });
+
+    it('should be the correct url', async () => {
+        expect(await page.url()).to.eql('https://google.com/');
+    });
+
+    it('should have the correct page title', async () => {
+        expect(await page.title()).to.eql('DuckDuckGo — Privacy, simplified.');
+    });
+
+    it('should have the page open', async () => {
+        expect(await page.isClosed()).to.eql(true);
+    });
+});
